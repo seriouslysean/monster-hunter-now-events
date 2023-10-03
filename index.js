883b6b9f1cbb44a0538ec03a9f76099f580e959c
Sean Kennedy <skennedy@urbn.com>
2023-10-02 20:48:42 -0400

    Add ascii art dinosaur

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import generateFeed from './src/generate-feed.js';
 
+/*
+
+                           __
+                          / _)  - Happy hunting!
+                 _/\/\/\_/ /
+               _|         /
+             _|  (  | (  |
+            /__.-'|_|--|_|
+
+*/
+
 generateFeed();
